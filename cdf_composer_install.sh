aae4bfc1dcc3d7663ce3b12b4934cdcdd0784d70
Callum <flemingcd@hotmail.com>
2021-05-01 10:53:01 +0200

    backup 01/05/2021 10:53:01

--- cdf_composer_install.sh
+++ cdf_composer_install.sh
@@ -7,5 +7,5 @@
     COMPOSER_HASH=`curl -sS https://composer.github.io/installer.sig` &&
     php -r "if (hash_file('SHA384', 'composer-setup.php') === '$COMPOSER_HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" | grep -q 'Installer verified' &&
     sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer &&
-    echo "✅ PHP + COMPOSER INSTALL COMPLETE" &&
+    echo "✅ PHP + COMPOSER INSTALL COMPLETE"
 )
